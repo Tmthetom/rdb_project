5de4aa6e85174315e4bee4813ce9a05336ac6b9e
Tmthetom <moravec-tomas@seznam.cz>
2017-05-13 09:35:42 +0200

    Reverse database settings

--- Application/Laptop Database/Laptop Database/Database/IDBConnector.cs
+++ Application/Laptop Database/Laptop Database/Database/IDBConnector.cs
@@ -9,7 +9,7 @@ namespace Laptop_Database.Database
 {
     public abstract class IDBConnector
     {
-        protected string server = "TOMASMORAVEC-PC\\SQLEXPRESS", database = "LaptopDatabase";
+        protected string server = "RONIKPC", database = "rdb_project";
         protected SqlConnection connection;
         
         protected bool success = false;
